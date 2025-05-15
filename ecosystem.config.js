23ce2022ac97bef8c55b6a9e1cecc707099b8c41
Nguyen Huu Tien <tien.nguyenhuu10@mobifone.vn>
2025-05-15 16:16:45 +0700

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "server",
+      script: "dist/index.js",
+      cwd: "./server",
+      watch: false,
+      instances: 1,
+      autorestart: true
+    },
+    {
+      name: "client",
+      script: "cmd",
+      args: "/c npm run start",
+      cwd: "./client",
+      watch: false,
+      instances: 1,
+      autorestart: true
+    }
+  ]
+}
